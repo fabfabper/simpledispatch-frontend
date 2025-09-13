7c20a593e21324fbc8f507c7c6c4e6c02a222a87
fabian.perler@ost.ch <fabian.perler@ost.ch>
2025-09-13 17:57:32 +0200

    change setting to new local c# api

--- src/api.js
+++ src/api.js
@@ -2,13 +2,13 @@
 // HTTP request functions for events and units
 
 export async function fetchEvents() {
-  const res = await fetch("http://localhost:3002/events");
+  const res = await fetch("http://localhost:5035/api/events");
   if (!res.ok) throw new Error("Failed to fetch events");
   return res.json();
 }
 
 export async function fetchUnits() {
-  const res = await fetch("http://localhost:3002/units");
+  const res = await fetch("http://localhost:5035/api/units");
   if (!res.ok) throw new Error("Failed to fetch units");
   return res.json();
 }
